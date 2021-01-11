cae161f186eb1e8f3c862d11e8408544820a4bca
Dan Hoover <mudmin@gmail.com>
2021-01-11 06:06:25 -0500

    splitting barcode by newline

---
+++ arduno-barcode/01-Initial-hackery.ino
@@ -0,0 +1,79 @@
+#include <hidboot.h>
+#include <usbhub.h>
+
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+#include <SPI.h>
+
+class KbdRptParser : public KeyboardReportParser
+{
+    void PrintKey(uint8_t mod, uint8_t key);
+
+  protected:
+    void OnControlKeysChanged(uint8_t before, uint8_t after);
+
+    void OnKeyDown	(uint8_t mod, uint8_t key);
+    void OnKeyUp	(uint8_t mod, uint8_t key);
+    void OnKeyPressed(uint8_t key);
+};
+
+void KbdRptParser::PrintKey(uint8_t m, uint8_t key)
+{
+  MODIFIERKEYS mod;
+  *((uint8_t*)&mod) = m;
+//if you'd rather have hex
+  PrintHex<uint8_t>(key, 0x80);
+
+};
+
+void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
+{
+//  Serial.print("DN ");
+  PrintKey(mod, key);
+  uint8_t c = OemToAscii(mod, key);
+
+  if (c)
+    OnKeyPressed(c);
+}
+
+void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
+  MODIFIERKEYS beforeMod;
+}
+
+void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
+{
+}
+
+void KbdRptParser::OnKeyPressed(uint8_t key)
+{
+//If you'd rather have ASCII
+//  Serial.println((char)key);
+};
+
+USB     Usb;
+//USBHub     Hub(&Usb);
+HIDBoot<USB_HID_PROTOCOL_KEYBOARD>    HidKeyboard(&Usb);
+
+KbdRptParser Prs;
+
+void setup()
+{
+  Serial.begin( 115200 );
+#if !defined(__MIPSEL__)
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+  Serial.println("Start");
+  if (Usb.Init() == -1)
+    Serial.println("OSC did not start.");
+
+  delay( 200 );
+
+  HidKeyboard.SetReportParser(0, &Prs);
+}
+
+void loop()
+{
+  Usb.Task();
+}
